445a79c303a36ea9c51b7b211c03acf72deb28d1
Artem Volovyi <artem.volovyi@nure.ua>
2023-11-03 11:40:48 +0200

    Add database project

---
+++ DeliveryBot/DeliveryBot.Db/BuildExtension.cs
@@ -0,0 +1,31 @@
+﻿using DeliveryBot.Db.DbContexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeliveryBot.Db;
+
+public static class BuildExtension
+{
+    public static void AddDbSetup(this IServiceCollection services, IConfiguration configuration)
+    {
+        var configurationString = configuration.GetRequiredSection("ConnectionString").Value;
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configurationString, b => b.MigrationsAssembly("DeliveryBot.Db")));
+
+        services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+    }
+}
---
+++ DeliveryBot/DeliveryBot.Db/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DeliveryBot.Db.DbContexts;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
