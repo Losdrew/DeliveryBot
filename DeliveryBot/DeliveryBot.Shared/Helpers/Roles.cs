c29e7e151c45da988f6eac6aba720201daaa5299
Artem Volovyi <artem.volovyi@nure.ua>
2023-11-06 17:57:20 +0200

    Add user roles

---
+++ DeliveryBot/DeliveryBot.Shared/Helpers/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace DeliveryBot.Shared.Helpers;
+
+public static class Roles
+{
+    public const string Admin = "admin";
+    public const string Manager = "manager";
+    public const string CompanyEmployee = "company-employee";
+    public const string Customer = "customer";
+}
