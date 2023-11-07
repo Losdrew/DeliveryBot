d5acc8b54679e4b83fe032d6939cdf51584d220e
Artem Volovyi <artem.volovyi@nure.ua>
2023-11-07 16:07:24 +0200

    Fix AccountError

--- DeliveryBot/DeliveryBot.Shared/ServiceErrors/AccountError.cs
+++ DeliveryBot/DeliveryBot.Shared/ServiceErrors/AccountError.cs
@@ -4,17 +4,10 @@ namespace DeliveryBot.Shared.ServiceErrors;
 
 public class AccountError : ServiceError
 {
-    public static AccountError IdentityCreateError = new()
+    public static AccountError AccountCreateError = new()
     {
         Header = "Account creation error",
         ErrorMessage = "Error when creating account",
         Code = 1
-    };
-
-    public static AccountError UserCreateError = new()
-    {
-        Header = "Account creation error",
-        ErrorMessage = "Error when creating account",
-        Code = 2
     };
 }
