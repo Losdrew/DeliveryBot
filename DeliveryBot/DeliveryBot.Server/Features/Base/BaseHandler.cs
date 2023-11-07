594b9604995608d8045a076cad7353f6d64f9f2e
Artem Volovyi <artem.volovyi@nure.ua>
2023-11-07 16:07:00 +0200

    Update BaseHandler

--- DeliveryBot/DeliveryBot.Server/Features/Base/BaseHandler.cs
+++ DeliveryBot/DeliveryBot.Server/Features/Base/BaseHandler.cs
@@ -5,5 +5,13 @@ namespace DeliveryBot.Server.Features.Base;
 public abstract class BaseHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
+    protected readonly ILogger<BaseHandler<TRequest, TResponse>> Logger;
+
+    protected BaseHandler(ILogger<BaseHandler<TRequest, TResponse>> logger)
+    {
+        Logger = logger;
+    }
+
     public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+    protected abstract Task<TResponse> UnsafeHandleAsync(TRequest request, CancellationToken cancellationToken);
 }
