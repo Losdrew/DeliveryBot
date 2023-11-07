28f9353da08d5e4a970d53d874d4206554e091c9
Artem Volovyi <artem.volovyi@nure.ua>
2023-11-07 16:53:20 +0200

    Add company employee sign up validation

--- DeliveryBot/DeliveryBot.Server/BuildInjections/ValidatorInjection.cs
+++ DeliveryBot/DeliveryBot.Server/BuildInjections/ValidatorInjection.cs
@@ -1,4 +1,4 @@
-﻿using DeliveryBot.Server.Features;
+﻿using DeliveryBot.Server.Features.Account;
 using DeliveryBot.Shared.Validators.Account;
 using FluentValidation;
 
@@ -9,5 +9,6 @@ internal static class ValidatorInjection
     internal static void AddValidators(this IServiceCollection services)
     {
         services.AddTransient<IValidator<CreateIdentityUserCommand>, CreateIdentityUserCommandValidator>();
+        services.AddTransient<IValidator<CreateCompanyEmployeeCommand>, CreateCompanyEmployeeCommandValidator>();
     }
 }
---
+++ DeliveryBot/DeliveryBot.Shared/Validators/Account/CreateCompanyEmployeeCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using DeliveryBot.Shared.Dto.Account;
+using FluentValidation;
+
+namespace DeliveryBot.Shared.Validators.Account;
+
+public class CreateCompanyEmployeeCommandValidator : AbstractValidator<CreateCompanyEmployeeCommandDto>
+{ 
+    public CreateCompanyEmployeeCommandValidator()
+    {
+        RuleFor(c => c.FirstName)
+            .NotNull();
+
+        RuleFor(c => c.LastName)
+            .NotNull();
+    }
+}
