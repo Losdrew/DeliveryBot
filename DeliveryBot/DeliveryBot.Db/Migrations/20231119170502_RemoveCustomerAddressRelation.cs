88ff121f492391039de17cc1be848d9035fe6c6a
Artem Volovyi <artem.volovyi@nure.ua>
2023-11-19 19:50:06 +0200

    Remove Customer-Address relation from database

---
+++ DeliveryBot/DeliveryBot.Db/Migrations/20231119170502_RemoveCustomerAddressRelation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryBot.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCustomerAddressRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Addresses_CustomerAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_CustomerAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerAddressId",
+                table: "Customers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustomerAddressId",
+                table: "Customers",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CustomerAddressId",
+                table: "Customers",
+                column: "CustomerAddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Addresses_CustomerAddressId",
+                table: "Customers",
+                column: "CustomerAddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+        }
+    }
+}
--- DeliveryBot/DeliveryBot.Db/Models/Customer.cs
+++ DeliveryBot/DeliveryBot.Db/Models/Customer.cs
@@ -3,6 +3,4 @@
 public class Customer : User
 {
     public string? PhoneNumber { get; set; }
-
-    public Address? CustomerAddress { get; set; }
 }
