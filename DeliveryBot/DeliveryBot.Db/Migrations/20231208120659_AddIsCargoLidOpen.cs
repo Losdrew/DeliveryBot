16dbfc5d7f5c8460e2679cb180ea7de44fe81cee
Artem Volovyi <artem.volovyi@nure.ua>
2023-12-08 14:19:16 +0200

    Add IsCargoLidOpen column in database

---
+++ DeliveryBot/DeliveryBot.Db/Migrations/20231208120659_AddIsCargoLidOpen.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryBot.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCargoLidOpen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCargoLidOpen",
+                table: "Robots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCargoLidOpen",
+                table: "Robots");
+        }
+    }
+}
