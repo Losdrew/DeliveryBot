f77e9efd0de6fe142ccea152ef8f40d01062b799
Artem Volovyi <artem.volovyi@nure.ua>
2023-12-10 23:28:07 +0200

    Add json bool converter

---
+++ DeliveryBot/DeliveryBot.Shared/Converters/BoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Formats.Asn1;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace DeliveryBot.Shared.Converters;
+
+public class BoolConverter : JsonConverter<bool>
+{
+    public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.TokenType switch
+        {
+            JsonTokenType.True => true,
+            JsonTokenType.False => false,
+            JsonTokenType.String => bool.TryParse(reader.GetString(), out var b) ? b : throw new JsonException(),
+            JsonTokenType.Number => reader.TryGetInt64(out long l) ? Convert.ToBoolean(l) : reader.TryGetDouble(out double d) && Convert.ToBoolean(d),
+            _ => throw new JsonException(),
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+    {
+        writer.WriteBooleanValue(value);
+    }
+}
--- DeliveryBot/DeliveryBot.Shared/Dto/Robot/UpdateRobotCommandDto.cs
+++ DeliveryBot/DeliveryBot.Shared/Dto/Robot/UpdateRobotCommandDto.cs
@@ -1,5 +1,7 @@
-﻿using DeliveryBot.Shared.Dto.Geolocation;
+﻿using DeliveryBot.Shared.Converters;
+using DeliveryBot.Shared.Dto.Geolocation;
 using DeliveryBot.Shared.Helpers;
+using System.Text.Json.Serialization;
 
 namespace DeliveryBot.Shared.Dto.Robot;
 
@@ -8,6 +10,7 @@ public class UpdateRobotCommandDto
     public string? DeviceId { get; set; }
     public RobotStatus Status { get; set; }
     public int BatteryCharge { get; set; }
+    [JsonConverter(typeof(BoolConverter))]
     public bool IsCargoLidOpen { get; set; }
     public LocationDto? Location { get; set; }
 }
