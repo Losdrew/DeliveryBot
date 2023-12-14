5ca64c406fca2fcde8fa694e29528412b4a867ff
Artem Volovyi <artem.volovyi@nure.ua>
2023-12-14 18:11:06 +0200

    Add login page

--- DeliveryBot/deliverybot.client/src/App.tsx
+++ DeliveryBot/deliverybot.client/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import RequireAuth from './components/RequireAuth';
+import { Login } from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
---
+++ DeliveryBot/deliverybot.client/src/pages/Login.tsx
@@ -0,0 +1,65 @@
+import { Button, Container, Link, Paper, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import authService from '../features/authService';
+import useAuth from '../hooks/useAuth';
+import { AuthResultDto, SignInCommand } from '../interfaces/account';
+
+export function Login() {
+  const { setAuth } = useAuth();
+  
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = async () => {
+    try {
+      const request: SignInCommand = { email, password };
+      const result: AuthResultDto = await authService.signIn(request);
+      setAuth(result);
+      navigate(from, { replace: true });
+    } catch (error) {
+      console.error('Error');
+    }
+  };
+
+  return (
+    <div className="bg-light pb-5" style={{ minHeight: "100vh" }}>
+      <Container component="main" maxWidth="xs">
+        <Paper elevation={3} style={{ padding: '30px', marginTop: '50px' }}>
+          <Typography variant="h5" gutterBottom align="center">
+            Sign in
+          </Typography>
+          <form>
+            <TextField
+              label="Email"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              label="Password"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Link href="/sign-up">
+              Create an account
+            </Link>
+            <Button variant="contained" color="primary" fullWidth style={{ marginTop: '20px' }} onClick={handleLogin}>
+              Sign in
+            </Button>
+          </form>
+        </Paper>
+      </Container>
+    </div>
+  );
+}
