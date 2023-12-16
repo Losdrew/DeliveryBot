d681e3debc14142312d48defb13dd1f2010b59ec
Artem Volovyi <artem.volovyi@nure.ua>
2023-12-17 01:44:21 +0200

    Update order service

--- DeliveryBot/deliverybot.client/src/features/orderService.ts
+++ DeliveryBot/deliverybot.client/src/features/orderService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import apiClient from "../config/apiClient";
-import { CreateOrderCommand, OrderInfoDto } from "../interfaces/order";
+import { CreateOrderCommand, OrderInfoDto, CancelOwnOrderCommand } from "../interfaces/order";
 
 const createOrder = async (
   request: CreateOrderCommand,
@@ -25,8 +25,54 @@ const createOrder = async (
   }
 };
 
+const getOwnOrders = async (
+  bearerToken: string,
+): Promise<OrderInfoDto[]> => {
+  try {
+    const headers = {
+      'Authorization': 'Bearer ' + bearerToken
+    };
+    const response = await apiClient.get<OrderInfoDto[]>(
+      '/api/Order/user-orders',
+      { headers }
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    } else {
+      throw new Error("Unknown error occurred.");
+    }
+  }
+};
+
+const cancelOrder = async (
+  request: CancelOwnOrderCommand,
+  bearerToken: string,
+): Promise<boolean> => {
+  try {
+    const headers = {
+      'Authorization': 'Bearer ' + bearerToken
+    };
+    await apiClient.post(
+      '/api/Order/user-orders',
+      request,
+      { headers }
+    );
+    return true;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    } else {
+      throw new Error("Unknown error occurred.");
+    }
+  }
+};
+
 const orderService = {
-  createOrder
+  createOrder,
+  getOwnOrders,
+  cancelOrder
 };
 
 export default orderService;
--- DeliveryBot/deliverybot.client/src/interfaces/order.ts
+++ DeliveryBot/deliverybot.client/src/interfaces/order.ts
@@ -1,10 +1,20 @@
 import { AddressDto } from "./address";
 import { OrderStatus } from "./enums";
+import { ProductDto } from "./product";
 
 export interface OrderInfoDto {
   placedDateTime?: Date;
   orderAddress?: AddressDto;
   orderProducts?: OrderProductDto[] | undefined;
+  id?: string;
+  customerId?: string;
+  orderStatus?: OrderStatus;
+}
+
+export interface OrderFullInfo {
+  placedDateTime?: Date;
+  orderAddress?: AddressDto;
+  products?: ProductDto[] | undefined;
   id?: string;
   customerId?: string;
   orderStatus?: OrderStatus;
