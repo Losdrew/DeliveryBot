76c45f0408ad568a675043297ecb8c5576771e0f
Artem Volovyi <artem.volovyi@nure.ua>
2023-12-16 15:09:54 +0200

    Fix auth provider

--- DeliveryBot/deliverybot.client/src/context/AuthProvider.tsx
+++ DeliveryBot/deliverybot.client/src/context/AuthProvider.tsx
@@ -1,33 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { AuthResultDto } from "../interfaces/account";
 
 interface AuthContextProps {
-    auth: AuthResultDto;
-    setAuth: React.Dispatch<React.SetStateAction<AuthResultDto>>;
+  auth: AuthResultDto;
+  setAuth: React.Dispatch<React.SetStateAction<AuthResultDto>>;
 }
 
 interface AuthProviderProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-    const [auth, setAuth] = useState<AuthResultDto>({});
+  const [auth, setAuth] = useState<AuthResultDto>({});
 
-    const [userId] = useState(localStorage.getItem("userId") || undefined);
-    const [bearer] = useState(localStorage.getItem("bearer") || undefined);
-    const [role] = useState(localStorage.getItem("role") || undefined);
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    const bearer = localStorage.getItem("bearer");
+    const role = localStorage.getItem("role");
 
-    if (userId && !auth.userId) {
-        setAuth({userId, bearer, role})
+    if (!auth.userId && userId && bearer && role) {
+      setAuth({userId, bearer, role})
     }
+  }, [auth.userId]);
 
-    return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export default AuthContext;
