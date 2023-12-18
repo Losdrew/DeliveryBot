f42c272ddc9c961303d3ebb182482e31f0e8a6b4
Artem Volovyi <artem.volovyi@nure.ua>
2023-12-18 14:17:33 +0200

    Update company service

--- DeliveryBot/deliverybot.client/src/features/companyService.ts
+++ DeliveryBot/deliverybot.client/src/features/companyService.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import apiClient from "../config/apiClient";
-import { CompanyPreviewDto } from "../interfaces/company";
+import { CompanyPreviewDto, CreateCompanyCommand, EditCompanyCommand, OwnCompanyInfoDto } from "../interfaces/company";
+import { CompanyEmployeeAccountDto } from "../interfaces/account";
+import { AddressDto } from "../interfaces/address";
 
 const getCompanies = async (
 ): Promise<CompanyPreviewDto[]> => {
@@ -18,8 +20,100 @@ const getCompanies = async (
   }
 };
 
+const getOwnCompany = async (
+  bearerToken : string
+): Promise<OwnCompanyInfoDto> => {
+  try {
+    const headers = {
+      'Authorization': 'Bearer ' + bearerToken
+    };
+    const response = await apiClient.get<OwnCompanyInfoDto>(
+      '/api/Company/user-company',
+      { headers }
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    } else {
+      throw new Error("Unknown error occurred.");
+    }
+  }
+};
+
+const createCompany = async (
+  name: string,
+  description: string,
+  websiteUrl: string,
+  companyAddresses: AddressDto[],
+  companyEmployees: CompanyEmployeeAccountDto[],
+  bearerToken : string
+): Promise<OwnCompanyInfoDto> => {
+  try {
+    const request: CreateCompanyCommand = { 
+      name,
+      description, 
+      websiteUrl, 
+      companyAddresses, 
+      companyEmployees 
+    };
+    const headers = {
+      'Authorization': 'Bearer ' + bearerToken
+    };
+    const response = await apiClient.post<OwnCompanyInfoDto>(
+      '/api/Company/create',
+      request,
+      { headers }
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    } else {
+      throw new Error("Unknown error occurred.");
+    }
+  }
+};
+
+const editCompany = async (
+  bearerToken : string,
+  name?: string,
+  description?: string,
+  websiteUrl?: string,
+  companyAddresses?: AddressDto[],
+  companyEmployees?: CompanyEmployeeAccountDto[],
+): Promise<OwnCompanyInfoDto> => {
+  try {
+    const request: EditCompanyCommand = { 
+      name,
+      description, 
+      websiteUrl, 
+      companyAddresses, 
+      companyEmployees 
+    };
+    const headers = {
+      'Authorization': 'Bearer ' + bearerToken
+    };
+    const response = await apiClient.post<OwnCompanyInfoDto>(
+      '/api/Company/edit',
+      request,
+      { headers }
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    } else {
+      throw new Error("Unknown error occurred.");
+    }
+  }
+};
+
 const companyService = {
-  getCompanies
+  getCompanies,
+  getOwnCompany,
+  createCompany,
+  editCompany
 };
 
 export default companyService;
